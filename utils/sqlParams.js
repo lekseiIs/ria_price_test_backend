abb2ecbf8787c0beb91b0ba4a114ac0aebd859e0
lekseiIs <liberateskeptic28@gmail.com>
2022-03-31 16:09:41 +0300

    Fixes

--- utils/sqlParams.js
+++ utils/sqlParams.js
@@ -15,6 +15,5 @@ module.exports = (body) => {
     .filter((param) => possibleParams[param[0]])
     .map((param) =>  possibleParams[param[0]] + '=' + param[1])
     .join(' and ');
-
   return `select year(publication_date) as year, month(publication_date) as month, avg(price) as avg from vehicle where date(publication_date) between date_sub(curdate(), interval 51 month) and date_sub(curdate(), interval 0 month) and ${formatedParams} group by year(publication_date), month(publication_date)`;
 };
