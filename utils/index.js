77e3272ed8b3bd26f2d6ec13cfea0aa2b3d2824f
OlehSydorenko25 <67220539+OlehSydorenko25@users.noreply.github.com>
2022-04-08 14:23:56 +0300

    replacement selectForm router

--- utils/index.js
+++ utils/index.js
@@ -1,13 +1,17 @@
 const urlGenerator = require('./urlGenerator');
 const months = require('./monthName');
-const { selectsForm, selectsModels, getById } = require('./selectsForm')
+const { getMarks, getRegions, getGearboxes, getFuelType, getBodyType, getModels, getById } = require('./selectsForm')
 const sqlGenerator = require('./sqlGenerator');
 
 module.exports = {
   urlGenerator, 
   sqlGenerator,
   months,
-  selectsForm,
-  selectsModels,
+  getMarks,
+  getRegions,
+  getGearboxes,
+  getFuelType,
+  getBodyType,
+  getModels,
   getById,
 };
